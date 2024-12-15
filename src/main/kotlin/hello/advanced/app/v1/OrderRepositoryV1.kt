6303a90fdf9e42bfca39a46b793dcf2841c3f8c4
heosangbeom <luv_3_u@naver.com>
2024-12-15 19:08:00 +0900

    ✨feat(log): V1 log 적용 -2
    - V1 log 적용 - 2

--- src/main/kotlin/hello/advanced/app/v1/OrderRepositoryV1.kt
+++ src/main/kotlin/hello/advanced/app/v1/OrderRepositoryV1.kt
@@ -1,17 +1,27 @@
 package hello.advanced.app.v1
 
+import hello.advanced.trace.TraceStatus
+import hello.advanced.trace.hellotrace.HelloTraceV1
 import org.springframework.stereotype.Repository
 import java.lang.Thread.sleep
 
 @Repository
-class OrderRepositoryV1 {
-
-    fun save(itemId: String){
-        // 저장 로직
-        if (itemId == "ex"){
-            throw IllegalStateException("예외 발생!")
+class OrderRepositoryV1(
+    private val trace: HelloTraceV1
+) {
+    fun save(itemId: String) {
+        var status: TraceStatus? = null
+        try {
+            status = trace.begin(" OrderRepository.save()")
+            // 저장 로직
+            if (itemId == "ex") {
+                throw IllegalStateException("예외 발생!")
+            }
+            sleep(1000)
+            trace.end(status)
+        } catch (e: Exception) {
+            trace.exception(status, e)
+            throw e
         }
-        sleep(1000)
     }
-
 }
--- src/main/kotlin/hello/advanced/app/v1/OrderServiceV1.kt
+++ src/main/kotlin/hello/advanced/app/v1/OrderServiceV1.kt
@@ -1,14 +1,25 @@
 package hello.advanced.app.v1
 
+import hello.advanced.trace.TraceStatus
+import hello.advanced.trace.hellotrace.HelloTraceV1
 import org.springframework.stereotype.Service
 
 @Service
 class OrderServiceV1(
-    private val orderRepository: OrderRepositoryV1
+    private val orderRepository: OrderRepositoryV1,
+    private val trace: HelloTraceV1
 ) {
 
-    fun orderItem(itemId: String){
-        orderRepository.save(itemId)
+    fun orderItem(itemId: String) {
+        var status: TraceStatus? = null
+        try {
+            status = trace.begin("OrderService.orderItem()")
+            orderRepository.save(itemId)
+            trace.end(status)
+        } catch (e: Exception) {
+            trace.exception(status, e)
+            throw e
+        }
     }
 
 }
